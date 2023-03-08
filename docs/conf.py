bfbca0337785cc5c9fb8b20da23086448ad10576
Aitzaz Imtiaz <aitzazimtiaz855@gmail.com>
2023-03-08 06:33:23 -0800

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -4,7 +4,7 @@ import sys
 sys.path.insert(0, os.path.abspath('..'))
 
 # -- Project information -------------------------------
-project = 'AitSad'
+project = 'SatClave'
 copyright = '2023, Aitzaz Imtiaz'
 author = 'Aitzaz Imtiaz'
 
